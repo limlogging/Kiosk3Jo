6ed8f6c18955e62db396db733f410571c2349315
SijongKim93 <rlatlwhd456@gmail.com>
2024-04-01 15:19:07 +0900

    데이터모델링 추가

---
+++ Kiosk/Models/DataManager.swift
@@ -0,0 +1,17 @@
+//
+//  DataManager.swift
+//  Kiosk
+//
+//  Created by 김시종 on 4/1/24.
+//
+
+import UIKit
+
+
+class DataManager {
+    var MacArray: [Mac] = []
+    var IphoneArray: [Iphone] = []
+    var Ipad: [Ipad] = []
+    var Watch: [Watch] = []
+    var Etc: [Etc] = []
+}
