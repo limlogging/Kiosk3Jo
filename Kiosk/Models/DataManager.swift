d18c2eaadca0063f362a1fbc7fc99861e0fa4d09
SijongKim93 <rlatlwhd456@gmail.com>
2024-04-01 15:30:22 +0900

    데이터 모델링 테스트 데이터 넣어둠

--- Kiosk/Models/DataManager.swift
+++ Kiosk/Models/DataManager.swift
@@ -11,7 +11,21 @@ import UIKit
 class DataManager {
     var MacArray: [Mac] = []
     var IphoneArray: [Iphone] = []
-    var Ipad: [Ipad] = []
-    var Watch: [Watch] = []
-    var Etc: [Etc] = []
+    var IpadArray: [Ipad] = []
+    var WatchArray: [Watch] = []
+    var EtcArray: [Etc] = []
+    
+    func makeProductData() {
+        MacArray = [Mac(title: "Mac", description: "test", price: 1000000),
+                    Mac(title: "test", description: "test", price: 1300000)]
+        
+        IphoneArray = [Iphone(title: "Iphone", description: "test", price: 10000000)]
+        
+        IpadArray = [Ipad(title: "Ipad", description: "test", price: 10900000)]
+        
+        WatchArray = [Watch(title: "Watch", description: "test", price: 10900000)]
+        
+        EtcArray = [Etc(title: "Etc", description: "test", price: 10900000)]
+    }
+    
 }
