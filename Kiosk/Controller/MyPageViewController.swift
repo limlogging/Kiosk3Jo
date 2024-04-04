543c0cef82649e0d9df79f9c88b913e275ec117d
imhs <limlogging@gmail.com>
2024-04-05 08:55:20 +0900

    테스트

--- Kiosk/Controller/MyPageViewController.swift
+++ Kiosk/Controller/MyPageViewController.swift
@@ -30,12 +30,81 @@ class MyPageViewController: UIViewController {
         return imageView
     }()
     
-    var profileView: UIView = {
+    var topBag: UIImageView = {
+        let imageView = UIImageView(image: UIImage(systemName: "bag"))
+        imageView.tintColor = .black
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    var topEqual: UIImageView = {
+        let imageView = UIImageView(image: UIImage(systemName: "equal"))
+        imageView.tintColor = .black
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    var accountView: UIView = {
         let view = UIView()
-        view.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        //view.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        //view.layer.borderWidth = 1
+        //view.layer.cornerRadius = 5
+        //view.clipsToBounds = true
+        view.backgroundColor = #colorLiteral(red: 0.960784018, green: 0.9607844949, blue: 0.9693934321, alpha: 1)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    var accountLabel: UILabel = {
+        let label = UILabel()
+        label.text = "계정"
+        label.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    var accountLoginStatusLabel: UILabel = {
+        let label = UILabel()
+        label.text = "로그아웃"
+        label.font = UIFont.systemFont(ofSize: 10)
+        label.textColor = .blue
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    var lineView: UIView = {
+        let view = UIView()
+        view.layer.borderColor = #colorLiteral(red: 0.8078431487, green: 0.8078433871, blue: 0.8121486306, alpha: 1)
         view.layer.borderWidth = 1
-        view.layer.cornerRadius = 5
-        view.clipsToBounds = true
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+        
+    var nameView: UIView = {
+        let view = UIView()
+        view.backgroundColor = #colorLiteral(red: 0.960784018, green: 0.9607844949, blue: 0.9693934321, alpha: 1)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    var myNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = ""
+        label.font = UIFont.systemFont(ofSize: 30, weight: .bold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    var view1: UIView = {
+        let view = UIView()
+        view.backgroundColor = #colorLiteral(red: 0.960784018, green: 0.9607844949, blue: 0.9693934321, alpha: 1)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    var view2: UIView = {
+        let view = UIView()
+        view.backgroundColor = #colorLiteral(red: 0.960784018, green: 0.9607844949, blue: 0.9693934321, alpha: 1)
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
@@ -101,7 +170,17 @@ class MyPageViewController: UIViewController {
         view.addSubview(topView)
         view.addSubview(topLogo)
         view.addSubview(topSearch)
-//        view.addSubview(profileView)
+        view.addSubview(topBag)
+        view.addSubview(topEqual)
+        view.addSubview(accountView)
+        view.addSubview(accountLabel)
+        view.addSubview(accountLoginStatusLabel)
+        view.addSubview(lineView)
+        view.addSubview(nameView)
+        view.addSubview(myNameLabel)
+        
+        view.addSubview(view1)
+        view.addSubview(view2)
 //        view.addSubview(profileImageView)
 //        view.addSubview(profileImageChangeButton)
         
@@ -109,30 +188,53 @@ class MyPageViewController: UIViewController {
             topView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             topView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
             topView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
-            topView.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.05),
+            topView.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.075),
             
             topLogo.centerYAnchor.constraint(equalTo: topView.centerYAnchor),
-            topLogo.leadingAnchor.constraint(equalTo: topView.leadingAnchor, constant: view.bounds.width * 0.1),
+            topLogo.leadingAnchor.constraint(equalTo: topView.leadingAnchor, constant: view.bounds.width * 0.05),
             
             topSearch.centerYAnchor.constraint(equalTo: topView.centerYAnchor),
-            topSearch.leadingAnchor.constraint(equalTo: topView.leadingAnchor, constant: view.bounds.width * 0.7)
+            topSearch.leadingAnchor.constraint(equalTo: topView.leadingAnchor, constant: view.bounds.width * 0.6),
             
-//            profileView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
-//            profileView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10),
-//            profileView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
-//            profileView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.2),
-//            
-//            profileImageView.topAnchor.constraint(equalTo: profileView.topAnchor, constant: 10 ),
-//            profileImageView.leadingAnchor.constraint(equalTo: profileView.leadingAnchor, constant: 10),
-//            //profileImageView.bottomAnchor.constraint(equalTo: profileView.bottomAnchor, constant: -10),
-//            //profileImageView.bottomAnchor.constraint(equalTo: profileImageChangeButton.topAnchor, constant: -10),
-//            profileImageView.widthAnchor.constraint(equalTo: profileView.widthAnchor, multiplier: 0.4),
-//            
-//            profileImageChangeButton.topAnchor.constraint(equalTo: profileImageView.bottomAnchor, constant: 5),
-//            profileImageChangeButton.leadingAnchor.constraint(equalTo: profileView.leadingAnchor, constant: 10),
-//            profileImageChangeButton.bottomAnchor.constraint(equalTo: profileView.bottomAnchor, constant: -10),
-//            profileImageChangeButton.heightAnchor.constraint(equalToConstant: 20),
-//            profileImageChangeButton.widthAnchor.constraint(equalTo: profileImageView.widthAnchor)
+            topBag.centerYAnchor.constraint(equalTo: topView.centerYAnchor),
+            topBag.leadingAnchor.constraint(equalTo: topView.leadingAnchor, constant: view.bounds.width * 0.75),
+            
+            topEqual.centerYAnchor.constraint(equalTo: topView.centerYAnchor),
+            topEqual.leadingAnchor.constraint(equalTo: topView.leadingAnchor, constant: view.bounds.width * 0.9),
+            
+            accountView.topAnchor.constraint(equalTo: topView.bottomAnchor),
+            accountView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            accountView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            accountView.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.075),
+            
+            accountLabel.centerYAnchor.constraint(equalTo: accountView.centerYAnchor),
+            accountLabel.leadingAnchor.constraint(equalTo: accountView.leadingAnchor, constant: view.bounds.width * 0.05),
+            
+            accountLoginStatusLabel.centerYAnchor.constraint(equalTo: accountView.centerYAnchor),
+            accountLoginStatusLabel.trailingAnchor.constraint(equalTo: accountView.trailingAnchor, constant: view.bounds.width * (-0.05)),
+            
+            lineView.topAnchor.constraint(equalTo: accountView.bottomAnchor),
+            lineView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            lineView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            lineView.heightAnchor.constraint(equalToConstant: 1),
+            
+            nameView.topAnchor.constraint(equalTo: lineView.bottomAnchor),
+            nameView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            nameView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            nameView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.15),
+            
+            myNameLabel.topAnchor.constraint(equalTo: nameView.topAnchor, constant: nameView.bounds.height * 0.5),
+            myNameLabel.leadingAnchor.constraint(equalTo: nameView.leadingAnchor, constant: nameView.bounds.width * 0.5),
+            
+            view1.topAnchor.constraint(equalTo: myNameLabel.bottomAnchor, constant: 10),
+            view1.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            view1.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            view1.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.15),
+            
+            view2.topAnchor.constraint(equalTo: view1.bottomAnchor, constant: 10),
+            view2.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            view2.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            view2.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.15)
         ])
     }
 }
