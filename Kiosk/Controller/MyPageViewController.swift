46034d70a3ae7c6d31bdc332bbe72a3151ff1f0c
imhs <limlogging@gmail.com>
2024-04-05 12:16:55 +0900

    마이페이지테이블뷰추가

--- Kiosk/Controller/MyPageViewController.swift
+++ Kiosk/Controller/MyPageViewController.swift
@@ -43,7 +43,6 @@ class MyPageViewController: UIViewController {
         imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
-    
     var accountView: UIView = {
         let view = UIView()
         //view.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
@@ -89,22 +88,45 @@ class MyPageViewController: UIViewController {
     
     var myNameLabel: UILabel = {
         let label = UILabel()
-        label.text = ""
+        label.text = "안녕하세요"
         label.font = UIFont.systemFont(ofSize: 30, weight: .bold)
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
-    var view1: UIView = {
+    var cartName: UILabel = {
+        let label = UILabel()
+        label.text = "장바구니"
+        label.font = UIFont.systemFont(ofSize: 30, weight: .bold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    // MARK: - 장바구니 테이블이 들어가는 뷰
+    var cartListView: UIView = {
         let view = UIView()
         view.backgroundColor = #colorLiteral(red: 0.960784018, green: 0.9607844949, blue: 0.9693934321, alpha: 1)
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
+        
+    // MARK: - 테이블 뷰 추가
+    var cartTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.layer.cornerRadius = 5
+        tableView.clipsToBounds = true
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+        
+    //-------------------------------------------------------------//
     
-    var view2: UIView = {
+    var profileView: UIView = {
         let view = UIView()
-        view.backgroundColor = #colorLiteral(red: 0.960784018, green: 0.9607844949, blue: 0.9693934321, alpha: 1)
+        view.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        view.layer.borderWidth = 1
+        view.layer.cornerRadius = 5
+        view.clipsToBounds = true
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
@@ -137,7 +159,19 @@ class MyPageViewController: UIViewController {
         super.viewDidLoad()
                 
         profileUI()
-        print(ListManager.shared.list)
+        //print(ListManager.shared.list)
+        
+        cartTableView.dataSource = self
+        cartTableView.rowHeight = 100
+        // 셀 클래스 등록
+        cartTableView.register(MyPageCartTableViewCell.self, forCellReuseIdentifier: "CartCellId")
+    }
+    
+    // MARK: - 테이블 뷰 새로고침
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        //마이페이지에 있다가 다시 상품으로 돌아가 카트를 추가하고 마이페이지로 돌아왔을때 화면변화가 없어서 추가해줘야함
+        cartTableView.reloadData()
     }
     
     @objc func changeImage(_ sender: UIButton) {
@@ -179,11 +213,10 @@ class MyPageViewController: UIViewController {
         view.addSubview(lineView)
         view.addSubview(nameView)
         view.addSubview(myNameLabel)
-        
-        view.addSubview(view1)
-        view.addSubview(view2)
-//        view.addSubview(profileImageView)
-//        view.addSubview(profileImageChangeButton)
+        //장바구니
+        view.addSubview(cartName)
+        view.addSubview(cartListView)
+        view.addSubview(cartTableView)
         
         NSLayoutConstraint.activate([
             topView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
@@ -219,23 +252,26 @@ class MyPageViewController: UIViewController {
             lineView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
             lineView.heightAnchor.constraint(equalToConstant: 1),
             
+            //회원정보
             nameView.topAnchor.constraint(equalTo: lineView.bottomAnchor),
             nameView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
             nameView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
             nameView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.15),
             
-            myNameLabel.topAnchor.constraint(equalTo: nameView.topAnchor, constant: nameView.bounds.height * 0.5),
-            myNameLabel.leadingAnchor.constraint(equalTo: nameView.leadingAnchor, constant: nameView.bounds.width * 0.5),
+            myNameLabel.topAnchor.constraint(equalTo: nameView.topAnchor, constant: 10),
+            myNameLabel.leadingAnchor.constraint(equalTo: nameView.leadingAnchor, constant: 10),
             
-            view1.topAnchor.constraint(equalTo: myNameLabel.bottomAnchor, constant: 10),
-            view1.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-            view1.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
-            view1.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.15),
-            
-            view2.topAnchor.constraint(equalTo: view1.bottomAnchor, constant: 10),
-            view2.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-            view2.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
-            view2.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.15)
+            //장바구니
+            cartName.topAnchor.constraint(equalTo: nameView.bottomAnchor, constant: view.bounds.height * 0.01),
+            cartName.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10),
+            cartListView.topAnchor.constraint(equalTo: cartName.bottomAnchor, constant: 2),
+            cartListView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            cartListView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            cartListView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            cartTableView.topAnchor.constraint(equalTo: cartListView.topAnchor, constant: 10),
+            cartTableView.leadingAnchor.constraint(equalTo: cartListView.leadingAnchor, constant: 10),
+            cartTableView.trailingAnchor.constraint(equalTo: cartListView.trailingAnchor, constant: -10),
+            cartTableView.bottomAnchor.constraint(equalTo: cartListView.bottomAnchor, constant: -10)
         ])
     }
 }
@@ -266,5 +302,25 @@ extension MyPageViewController: UIImagePickerControllerDelegate {
         }
 
         self.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension MyPageViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("ListManager.shared.list.count: \(ListManager.shared.list.count)")
+        return ListManager.shared.list.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CartCellId", for: indexPath) as? MyPageCartTableViewCell
+        
+        cell?.productImage.image = ListManager.shared.list[indexPath.row].image
+        cell?.productName.text = ListManager.shared.list[indexPath.row].name
+        cell?.productPrice.text = String(ListManager.shared.list[indexPath.row].price) + "원"
+        cell?.productValue.text = String(ListManager.shared.list[indexPath.row].value) + "개"
+        
+        print(ListManager.shared.list)
+        
+        return cell ?? UITableViewCell()
     }
 }
